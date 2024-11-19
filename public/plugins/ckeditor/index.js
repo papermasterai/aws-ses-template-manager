26d76d36fa8b6f960038c35cd4ec765081195d57
TangFan <yanniluo@192.168.20.19>
2024-11-19 15:22:59 +0800

    feat: add ckeditor5 for email template edit

---
+++ public/plugins/ckeditor/index.js
@@ -0,0 +1,272 @@
+const {
+	ClassicEditor,
+	AccessibilityHelp,
+	Autoformat,
+	Alignment,
+	AutoImage,
+	Autosave,
+	BlockQuote,
+	Bold,
+	FontColor,
+	FontSize,
+	Essentials,
+	FindAndReplace,
+	Heading,
+	Highlight,
+	ImageBlock,
+	ImageCaption,
+	ImageInline,
+	ImageInsertViaUrl,
+	ImageResize,
+	ImageStyle,
+	ImageTextAlternative,
+	ImageToolbar,
+	Indent,
+	IndentBlock,
+	Italic,
+	Link,
+	LinkImage,
+	List,
+	ListProperties,
+	Markdown,
+	MediaEmbed,
+	Paragraph,
+	PasteFromMarkdownExperimental,
+	PasteFromOffice,
+	SelectAll,
+	Strikethrough,
+	Table,
+	TableCaption,
+	TableCellProperties,
+	TableColumnResize,
+	TableProperties,
+	TableToolbar,
+	TextTransformation,
+	TodoList,
+	Underline,
+	Undo
+} = CKEDITOR;
+
+
+const CKEditorConfig = {
+	toolbar: {
+		items: [
+			'undo',
+			'redo',
+			'|',
+			'findAndReplace',
+			'|',
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'underline',
+			'strikethrough',
+			'fontsize',
+			'fontcolor',
+			'alignment',
+			'|',
+			'link',
+			'insertImageViaUrl',
+			'mediaEmbed',
+			'insertTable',
+			'highlight',
+			'blockQuote',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'todoList',
+			'outdent',
+			'indent'
+		],
+		shouldNotGroupWhenFull: false
+	},
+	plugins: [
+		AccessibilityHelp,
+		Autoformat,
+		Alignment,
+		AutoImage,
+		Autosave,
+		BlockQuote,
+		Bold,
+		FontSize,
+		FontColor,
+		Essentials,
+		FindAndReplace,
+		Heading,
+		Highlight,
+		ImageBlock,
+		ImageCaption,
+		ImageInline,
+		ImageInsertViaUrl,
+		ImageResize,
+		ImageStyle,
+		ImageTextAlternative,
+		ImageToolbar,
+		Indent,
+		IndentBlock,
+		Italic,
+		Link,
+		LinkImage,
+		List,
+		ListProperties,
+		MediaEmbed,
+		Paragraph,
+		PasteFromMarkdownExperimental,
+		PasteFromOffice,
+		SelectAll,
+		Strikethrough,
+		Table,
+		TableCaption,
+		TableCellProperties,
+		TableColumnResize,
+		TableProperties,
+		TableToolbar,
+		TextTransformation,
+		TodoList,
+		Underline,
+		Undo
+	],
+	heading: {
+		options: [
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'heading1',
+				view: 'h1',
+				title: 'Heading 1',
+				class: 'ck-heading_heading1'
+			},
+			{
+				model: 'heading2',
+				view: 'h2',
+				title: 'Heading 2',
+				class: 'ck-heading_heading2'
+			},
+			{
+				model: 'heading3',
+				view: 'h3',
+				title: 'Heading 3',
+				class: 'ck-heading_heading3'
+			},
+			{
+				model: 'heading4',
+				view: 'h4',
+				title: 'Heading 4',
+				class: 'ck-heading_heading4'
+			},
+			{
+				model: 'heading5',
+				view: 'h5',
+				title: 'Heading 5',
+				class: 'ck-heading_heading5'
+			},
+			{
+				model: 'heading6',
+				view: 'h6',
+				title: 'Heading 6',
+				class: 'ck-heading_heading6'
+			}
+		]
+	},
+	image: {
+		resizeUnit: 'px', 
+		styles: {
+			// 禁用 aspect-ratio 属性
+			// 改为使用 width 和 height 属性
+			full: {
+				name: 'Full size',
+				icon: 'full',
+				attributes: {
+					width: '100%',
+					height: 'auto'
+				}
+			},
+			side: {
+				name: 'Side image',
+				icon: 'side',
+				attributes: {
+					width: '50%',
+					height: 'auto'
+				}
+			},
+			alignLeft: {
+				name: 'Align left',
+				icon: 'align-left',
+				attributes: {
+					width: '50%',
+					height: 'auto'
+				}
+			},
+			alignRight: {
+				name: 'Align right',
+				icon: 'align-right',
+				attributes: {
+					width: '50%',
+					height: 'auto'
+				}
+			}
+		},
+		resizeOptions: [
+			{
+				name: 'resizeImage:original',
+				value: null,
+				icon: 'original'
+			},
+			{
+				name: 'resizeImage:50',
+				value: '50',
+				icon: 'medium'
+			},
+			{
+				name: 'resizeImage:75',
+				value: '75',
+				icon: 'large'
+			}
+		],
+		toolbar: [
+			'toggleImageCaption',
+			'imageTextAlternative',
+			'|',
+			'imageStyle:inline',
+			'imageStyle:wrapText',
+			'imageStyle:breakText',
+			'|',
+			'resizeImage'
+		]
+	},
+
+	language: 'zh-cn',
+	link: {
+		addTargetToExternalLinks: true,
+		defaultProtocol: 'https://',
+		decorators: {
+			toggleDownloadable: {
+				mode: 'manual',
+				label: 'Downloadable',
+				attributes: {
+					download: 'file'
+				}
+			}
+		}
+	},
+	list: {
+		properties: {
+			styles: true,
+			startIndex: true,
+			reversed: true
+		}
+	},
+	menuBar: {
+		isVisible: true
+	},
+	placeholder: '请输入邮件正文!',
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+	},
+	translations: [CKEDITOR_TRANSLATIONS]
+};
+
