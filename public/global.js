32cf44ed55126ddcea0bce0dc6b2dbbec6f8be59
TangFan <yanniluo@YannideMacBook-Pro.local>
2024-12-02 09:27:19 +0800

    feat: add line height for ckeditor5

--- public/global.js
+++ public/global.js
@@ -10,7 +10,7 @@ function populateTextSectionContent() {
 }
 
 (async function () {
-  window.CK_IMAGE_STYLE_STRING = '<style>img {max-width: 100%;height: auto;}</style>'
+  window.CK_IMAGE_STYLE_STRING = '<style>img {max-width: 100%;height: auto;} p{line-height: 1.6;}</style>'
   const versionChecked = sessionStorage.getItem('versionChecked');
   if (!versionChecked) {
     await $.get(`https://api.github.com/repos/MattRuddick/aws-ses-template-manager/tags`, (response) => {
